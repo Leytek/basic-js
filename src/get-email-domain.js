437f7d576375482a214bfa7d3601d5aac92d46bc
Leytek <leytek@yandex.ru>
2022-05-26 20:41:02 +0300

    refactor: drop regExp

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -11,7 +11,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function getEmailDomain(s) {
-  return s.match(/(?<=@)[^@]+$/)[0];
+  return s.split`@`.slice(-1)[0];
 }
 
 module.exports = {
